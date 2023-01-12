4785f68ac4a7b0dd9a2b8a52c5edbc0b10a9ac3f
zbyju <zbynek.jurica@gmail.com>
2023-01-12 20:19:31 +0200

    Add safelist of generated colors

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const generatedSafelist = ["blue", "pink"].reduce((safelist, color) => {
+  const weights = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+  const bgs = weights.map((w) => `bg-${color}-${w}`);
+  const hoverBgs = weights.map((w) => `hover:bg-${color}-${w}`);
+  const texts = weights.map((w) => `text-${color}-${w}`);
+  return safelist.concat(bgs, hoverBgs, texts);
+}, []);
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  safelist: generatedSafelist,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
