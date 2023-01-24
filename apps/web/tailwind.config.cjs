52d9603a30780fa4ba6b3b8e6086650dc05f7566
zbyju <zbynek.jurica@gmail.com>
2023-01-24 11:40:45 +0200

    Add xs screen to tailwind

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -40,6 +40,14 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   safelist: [].concat(generatedSafelist, generateBasicSafelist),
   theme: {
+    screens: {
+      xs: "420px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {},
   },
   plugins: [],
